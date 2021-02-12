6c61e01f055b7f1b0a9e677f7308d865f82ea47c
Abir Taheer <abir@taheer.me>
2021-02-11 19:24:32 -0500

    added og picture and description

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,16 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <script src={'https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js'} />
                 <script src={'https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js'} />
+                <meta
+                    property={'og:image'}
+                    content={
+                        'https://res.cloudinary.com/hoxqr9glj/image/upload/v1613031770/10_j2b7r7.png'
+                    }
+                />
+                <meta
+                    property={'og:description'}
+                    content={"Send Valentin's day letters to your friends, even anonymously!"}
+                />
             </Head>
             <Backdrop />
 
