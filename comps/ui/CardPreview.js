57e28ceb120b2258f5ebf03d5081e9b899027bc6
Abir Taheer <abir@taheer.me>
2021-02-11 22:39:06 -0500

    made card movements verical again because of content getting prematurely exposed

--- comps/ui/CardPreview.js
+++ comps/ui/CardPreview.js
@@ -46,7 +46,7 @@ const CardPreview = ({ variant, offset, textColor, backgroundColor, url, message
                     This card is double-sided. Click on it to see the other side.
                 </p>
                 <br />
-                <ReactCardFlip isFlipped={flipped} flipDirection='horizontal'>
+                <ReactCardFlip isFlipped={flipped} flipDirection='vertical'>
                     <div
                         style={{
                             width: '95vw',
